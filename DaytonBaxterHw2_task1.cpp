c8058bce8abda6aef441a25618febf668da68f2a
daytonbaxter <baxterd23@gmail.com>
2017-09-19 13:00:10 -0600

    Initial commit

---
+++ DaytonBaxterHw2_task1.cpp
@@ -0,0 +1,21 @@
+// File: DaytonBaxterHw2_task1
+// Created by Dayton Baxter on 9/19/2017 for CS1410
+// Copyright (c) 2017 WSU
+
+#include <iostream>
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+int main()
+{
+
+
+    return 0;
+
+}
+
+// Function Definitions
